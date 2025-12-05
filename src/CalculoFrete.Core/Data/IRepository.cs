9f16c34cf3c7461e4511bbfb64dfcd04614eb9a8
= <=>
2025-12-05 15:58:29 -0300

    Criação do In-Memory Repository

---
+++ src/CalculoFrete.Core/Data/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using CalculoFrete.Core.Domain;
+
+namespace CalculoFrete.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : Entity
+    {
+        Task<T?> ObterPorIdAsync(Guid id);
+        Task<IEnumerable<T>> ObterTodosAsync();
+        void Adicionar(T entity);
+        void Atualizar(T entity);
+        void Remover(Guid id);
+    }
+}
---
+++ src/CalculoFrete.Core/Data/InMemoryRepository.cs
@@ -0,0 +1,39 @@
+﻿using CalculoFrete.Core.Domain;
+
+namespace CalculoFrete.Core.Data
+{
+    public class InMemoryRepository<T> : IRepository<T> where T : Entity
+    {
+        protected readonly Dictionary<Guid, T> _storage = new();
+
+        public void Adicionar(T entity)
+        {
+            _storage.Add(entity.Id, entity);
+        }
+
+        public void Atualizar(T entity)
+        {
+            _storage[entity.Id] = entity;
+        }
+
+        public Task<T?> ObterPorIdAsync(Guid id)
+        {
+            return Task.FromResult(_storage.TryGetValue(id, out var entity) ? entity : null);
+        }
+
+        public Task<IEnumerable<T>> ObterTodosAsync()
+        {
+            return Task.FromResult(_storage.Select(x => x.Value));
+        }
+
+        public void Remover(Guid id)
+        {
+            _storage.Remove(id);
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
